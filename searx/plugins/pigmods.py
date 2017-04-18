dee93e502765802b3eb05c4751f2344cb3cf436c
lapigr <pig@lapig.xyz>
2017-04-18 15:58:18 +0000

    fork my abomination

---
+++ searx/plugins/pigmods.py
@@ -0,0 +1,53 @@
+'''
+searx is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+searx is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with searx. If not, see < http://www.gnu.org/licenses/ >.
+
+(C) 2015 by Adam Tauber, <asciimoo@gmail.com>
+'''
+from flask_babel import gettext
+import re
+name = "Pig search criteria handlers"
+description = gettext('Try to handle mail tracking numbers properly')
+default_on = True
+
+
+# Self User Agent regex
+p = re.compile('[0-9]{20,}')
+
+
+# attach callback to the post search hook
+#  request: flask request object
+#  ctx: the whole local context of the pre search hook
+def post_search(request, search):
+    if search.search_query.pageno > 1:
+        return True
+#    if search.search_query.query == 'ip':
+#        x_forwarded_for = request.headers.getlist("X-Forwarded-For")
+#        if x_forwarded_for:
+#            ip = x_forwarded_for[0]
+#        else:
+#            ip = request.remote_addr
+#        search.result_container.answers.clear()
+#        search.result_container.answers.add(ip)
+#    if p.match(search.search_query.query):
+#        ua = "<a href='https://google.com/#safe=off&q=" + search.search_query.query + "'/>google</a>"
+#        search.result_container.answers.clear()
+#        search.result_container.answers.add(ua)
+    return True
+
+
+def on_result(request, search, result):
+    if p.match(search.search_query.query):
+        result['url'] = 'https://google.com/&q=' + search.search_query.query
+        result['parsed_url'] = urlparse(result['url'])
+    return True
